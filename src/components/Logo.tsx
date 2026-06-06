95e6e0f10fa067999e14d019b5c61d9499b087a2
ridwantajudeen <ridwantajudeen8@gmail.com>
2026-06-06 16:20:24 +0100

    login update

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,11 @@
 import { Link } from "@tanstack/react-router";
-import logoAsset from "@/assets/guorrow-logo.png.asset.json";
 
 export function Logo({ to = "/", className = "h-7" }: { to?: string; className?: string }) {
   return (
     <Link to={to} className="inline-flex items-center group">
       <img
-        src={logoAsset.url}
-        alt="guorrow"
+        src="/GuorrowLogo.svg"
+        alt="Guorrow"
         className={`${className} w-auto object-contain`}
       />
     </Link>
