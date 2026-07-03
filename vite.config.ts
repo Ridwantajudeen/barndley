dbde473d5e3a3d64baa6975f37ed363b131e0e10
ridwantajudeen <ridwantajudeen8@gmail.com>
2026-07-03 11:44:21 +0100

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 
@@ -15,7 +14,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    nitro(),
     react(),
     tsconfigPaths(),
     tailwindcss(),
